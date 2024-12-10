500f549e7a891798e615a3215e44aa409398d64d
NitinNegi <nitinnegi3112h@gmail.com>
2024-12-10 16:16:23 +0530

    Image alt meaning added sma

--- src/pages/Page1.js
+++ src/pages/Page1.js
@@ -1,122 +1,122 @@
-import React from 'react'
+import React from 'react';
 import { CiHeart } from "react-icons/ci";
 import Counter from './Counter';
 import { IoAddOutline } from "react-icons/io5";
-import Carousel from "./Carousel"
 import Image from 'next/image';
 
-
 function page1() {
-   
-
-
   return (
     <div>
-       <div className='my-4 ml-4 text-gray-500'><span className='font-semibold text-black'>Home </span>{' >' } Art de la table </div>
-       
-       <div className='flex flex-col  w-[100%]  sm:h-[500px] md:h-[400px] gap-5 sm:gap-2 sm:items-start sm:flex-row md:flex-row  items-center   lg:h-[500px] lg:justify-around md:justify-between '>
-         
-         <div className=' w-[100%] mr-1 sm:h-[85%]   flex justify-center md:none lg:w-[50%] xl:w-[50%] md:w-[40%]  '>
-          
-          <Image   width={300}    alt="image1"
-                    height={100} src='/page1image.png'    className=' w-[90%] sm:w-[100%] sm:h-[90%] md:w-[100%] md:h-[340px]  lg:w-[90%] lg:h-[95%]    object-cover ml-10 rounded-lg'/>
-         </div>
+      <div className="my-4 ml-4 text-gray-500">
+        <span className="font-semibold text-black">Home </span>
+        {' >'} Art de la table
+      </div>
 
-         <div className='w-[100%] h-[400px] sm:h-[380px]   md:h-[85%] flex flex-col items-center md:w-[50%]'>
-            
-            <div className='w-[80%]  h-[83%] md:w-[90%] border-b-2 sm:text-[12px] '>
-              <div className='flex items-center justify-stretch border-b-2 pb-2'> 
-                <div>
-                   <h1 className='text-[18px] sm:text-[18px] lg:text-[30px] font-serif md:text-[20px]'>Cheese appareil à raclette 1/2 roue</h1>
-                   <n className=" text-[15px] sm:text-[15px] md:text-[20px] lg:text-[25px] font-semibold">39,50€ <ni className="font-normal lg:text-[20px] text-gray-600">/pièce</ni></n>
-                </div>
+      <div className="flex flex-col w-[100%] sm:h-[500px] md:h-[400px] gap-5 sm:gap-2 sm:items-start sm:flex-row md:flex-row items-center lg:h-[500px] lg:justify-around md:justify-between">
+        <div className="w-[100%] mr-1 sm:h-[85%] flex justify-center md:none lg:w-[50%] xl:w-[50%] md:w-[40%]">
+          <Image
+            width={300}
+            height={100}
+            alt="Cheese appareil image"
+            src="/page1image.png"
+            className="w-[90%] sm:w-[100%] sm:h-[90%] md:w-[100%] md:h-[340px] lg:w-[90%] lg:h-[95%] object-cover ml-10 rounded-lg"
+          />
+        </div>
 
-                <CiHeart className='ml-auto text-[30px] sm:text-[25px] md:text-[25px] lg:text-[35px]'/>
-              
+        <div className="w-[100%] h-[400px] sm:h-[380px] md:h-[85%] flex flex-col items-center md:w-[50%]">
+          <div className="w-[80%] h-[83%] md:w-[90%] border-b-2 sm:text-[12px]">
+            <div className="flex items-center justify-stretch border-b-2 pb-2">
+              <div>
+                <h1 className="text-[18px] sm:text-[18px] lg:text-[30px] font-serif md:text-[20px]">
+                  Cheese appareil à raclette 1/2 roue
+                </h1>
+                <n className="text-[15px] sm:text-[15px] md:text-[20px] lg:text-[25px] font-semibold">
+                  39,50€ <ni className="font-normal lg:text-[20px] text-gray-600">/pièce</ni>
+                </n>
               </div>
-               
-              <div className='flex py-4 items-center justify-stretch border-b-2 md:text-[15px] '> 
-                <div className='flex gap-5 '>
-                
-                <div className='flex gap-2 items-center '>
-                   <Image   width={20}    alt="image2"
-                    height={100} src='/Capa_1.png'  />
-                  <span >20 <sup  className='font-semibold'>cm</sup></span>
+              <CiHeart className="ml-auto text-[30px] sm:text-[25px] md:text-[25px] lg:text-[35px]" />
+            </div>
+
+            <div className="flex py-4 items-center justify-stretch border-b-2 md:text-[15px]">
+              <div className="flex gap-5">
+                <div className="flex gap-2 items-center">
+                  <Image
+                    width={20}
+                    height={100}
+                    alt="Dimension icon 20cm"
+                    src="/Capa_1.png"
+                  />
+                  <span>
+                    20 <sup className="font-semibold">cm</sup>
+                  </span>
                 </div>
 
-                <div className='flex gap-2 items-center'>
-                   <Image   width={20}    alt="image3"
-                    height={100} src='/Frame.png'    />
-                  <span >50 <sup  className='font-semibold'>cm</sup></span>
+                <div className="flex gap-2 items-center">
+                  <Image
+                    width={20}
+                    height={100}
+                    alt="Dimension icon 50cm"
+                    src="/Frame.png"
+                  />
+                  <span>
+                    50 <sup className="font-semibold">cm</sup>
+                  </span>
                 </div>
-                
-                </div>
-
-                <n className="ml-auto text-[#868484]" >REF: VABGN5</n>
-              
               </div>
-               
-              <div className='pt-4 md:text-[12px] lg:text-[15px]'>
-              
+
+              <n className="ml-auto text-[#868484]">REF: VABGN5</n>
+            </div>
+
+            <div className="pt-4 md:text-[12px] lg:text-[15px]">
               <ul>
-                <li>Location appareil à raclette - Raclette traditionnelle 1/2 roue </li>
-                <li> Réglable en hauteur</li>
+                <li>Location appareil à raclette - Raclette traditionnelle 1/2 roue</li>
+                <li>Réglable en hauteur</li>
                 <li>Appareil à raclette professionnel</li>
                 <li>Boîtier de chauffe horizontal réglable en hauteur</li>
-                <li className='pt-4'> 220V </li>
-                <li>  900W </li>
+                <li className="pt-4">220V</li>
+                <li>900W</li>
               </ul>
-      
-              </div>
-
-               
-             </div>
-            
-            <div className='flex gap-2 mt-2 md:gap-2 '>
-              <Counter/>
-
-              <button className='w-[80%]  md:w-[60%] lg:w-[80%] rounded-md bg-[#5CD2DD] text-white'>AJOUTER AU PANIER</button>
-
-              
-             </div>
-
-         </div>
-
-
-       </div>
-
-       <div className='w-[100%] h-[20%] flex md:flex-row flex-col md:mt-0 mt-2 mb-10 '> 
-       
-        <div className='w-[85%] md:w-[43.4%] ml-10 flex flex-col gap-3'>
-           <p className='font-semibold text-[20px] font-sans'>Description produit</p>
-           <n className="text-[#3b3b3b]">Festi vous propose à la location un/une "Jewel-grand couteau/10pc pour votre évenement et ce dès 0,35 € /pièce (HTVA). Que ce soit pour votre mariage, une fête d'anniversaire ou du personnel, ce produit a fait l'objet d'une sélection rigoureuse par notre équipe. Il est en location chez nous sous la référence "VAJGC". Nous sommes à votre disposition pour que les événements de nos clients, même en last-minute, soient toujours une réussite. Vous pourrez trouver tout une série d'autre produit à louer de ce type dans la catégorie "Art de la Table".</n>
-        </div>
-          
-          <div className='w-[95%] ml-8  md:w-[50%] md:ml-20 md:mt-5 mt-10  flex flex-col '>
-              
-              <div className='w-[90%] h-[70px] bg-[#EEEBEB] flex items-center px-6 p-2'>
-                <n>LIVRAISONS</n>
-                <IoAddOutline className='ml-auto text-[25px]'/>
-              </div>
-
-              <div className='w-[90%] h-[70px] bg-[#EEEBEB] flex items-center px-6 p-2'>
-                <n>QUESTIONS</n>
-                <IoAddOutline className='ml-auto text-[25px]'/>
-              </div>
-
+            </div>
           </div>
 
-        <div>
-
+          <div className="flex gap-2 mt-2 md:gap-2">
+            <Counter />
+            <button className="w-[80%] md:w-[60%] lg:w-[80%] rounded-md bg-[#5CD2DD] text-white">
+              AJOUTER AU PANIER
+            </button>
+          </div>
         </div>
-         
+      </div>
 
-       </div>
-     
-   
+      <div className="w-[100%] h-[20%] flex md:flex-row flex-col md:mt-0 mt-2 mb-10">
+        <div className="w-[85%] md:w-[43.4%] ml-10 flex flex-col gap-3">
+          <p className="font-semibold text-[20px] font-sans">Description produit</p>
+          <n className="text-[#3b3b3b]">
+            Festi vous propose à la location un/une &quot;Jewel-grand couteau/10pc pour
+            votre évenement et ce dès 0,35 € /pièce (HTVA). Que ce soit pour votre
+            mariage, une fête d&apos;anniversaire ou du personnel, ce produit a fait
+            l&apos;objet d&apos;une sélection rigoureuse par notre équipe. Il est en
+            location chez nous sous la référence &quot;VAJGC&quot;. Nous sommes à votre
+            disposition pour que les événements de nos clients, même en last-minute,
+            soient toujours une réussite. Vous pourrez trouver tout une série d&apos;autre
+            produit à louer de ce type dans la catégorie &quot;Art de la Table&quot;.
+          </n>
+        </div>
 
+        <div className="w-[95%] ml-8 md:w-[50%] md:ml-20 md:mt-5 mt-10 flex flex-col">
+          <div className="w-[90%] h-[70px] bg-[#EEEBEB] flex items-center px-6 p-2">
+            <n>LIVRAISONS</n>
+            <IoAddOutline className="ml-auto text-[25px]" />
+          </div>
+
+          <div className="w-[90%] h-[70px] bg-[#EEEBEB] flex items-center px-6 p-2">
+            <n>QUESTIONS</n>
+            <IoAddOutline className="ml-auto text-[25px]" />
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default page1
+export default page1;
--- src/pages/Page3.js
+++ src/pages/Page3.js
@@ -1,83 +1,91 @@
-import React from 'react'
+import React from 'react';
 import Image from 'next/image';
 
 function Page3() {
   return (
-    <div className=' bg-[#FEF8FB]   relative'>
-        
-        <div className='w-full flex items-center mt-10 gap-7 md:gap-0 justify-around md:flex-row flex-col'>
-           
-           <div className='w-[90%]  md:w-[45%] bg items-center'>
-          <Image   width={300}    alt="image8"
-                    height={100} src="/BUFFET.png"    className='w-[300rem] h-[300px] md:w-[2500px] md:h-[23rem] lg:h-[17rem] ml-3 rounded-lg' alt="image" />
-           </div>
-
-           <div className='w-[90%]  md:w-[45%]  flex flex-col bg-[#f8d9e98d] p-5 rounded-lg gap-4'>
-             <p className='text-[25px] md:text-[36px]'>S'inscrire & économiser <span className='text-[#2ee2fe]'>10%</span></p>
-             <p className='text-[15px] text-[#6c6b6b]'>Office ipsum you must be muted. Synergize helicopter prioritize anyway job due harvest most opportunity didn't. Yet busy any meeting shark light marginalised 4-blocker message. Productize corporate nail caught synergy highlights lunch. Company another pushback items dear or any</p>
-
-             <div className='flex mt-5 justify-around'>
-              <input type='text' className='w-[60%] h-[50px] rounded-md placeholder:pl-4' placeholder='john@doe.com'/>
-              <button className='bg-[#42ccd9] text-white font-semibold p-3 rounded-lg'>S'INSCRIRE {`->`}</button>
-
-              </div>  
-           </div>
-
+    <div className="bg-[#FEF8FB] relative">
+      <div className="w-full flex items-center mt-10 gap-7 md:gap-0 justify-around md:flex-row flex-col">
+        <div className="w-[90%] md:w-[45%] items-center">
+          <Image
+            width={300}
+            height={100}
+            alt="Buffet setup"
+            src="/BUFFET.png"
+            className="w-[300rem] h-[300px] md:w-[2500px] md:h-[23rem] lg:h-[17rem] ml-3 rounded-lg"
+          />
         </div>
 
+        <div className="w-[90%] md:w-[45%] flex flex-col bg-[#f8d9e98d] p-5 rounded-lg gap-4">
+          <p className="text-[25px] md:text-[36px]">
+            S&#39;inscrire & économiser <span className="text-[#2ee2fe]">10%</span>
+          </p>
+          <p className="text-[15px] text-[#6c6b6b]">
+            Office ipsum you must be muted. Synergize helicopter prioritize anyway job due harvest most
+            opportunity didn&#39;t. Yet busy any meeting shark light marginalised 4-blocker message.
+            Productize corporate nail caught synergy highlights lunch. Company another pushback items dear or any.
+          </p>
 
-        <div className='mt-10 border-t-2 pt-4 h-[400px] flex flex-col w-[100%] items-center gap-10 justify-between
-        '>
-       
-          <div className='flex w-[100%] flex-col items-center gap-4'>
-          <Image   width={300}    alt="image7"
-                    height={100} src='/logowebsie.png'     className='w-[100px] object-contain   h-[100px]'/>
-
-          <div className='flex w-[80%] justify-evenly gap-3 text-[10px] '>
-            <div className='text-sm text-[9px]'>
-              <p className=' text-[11px] font-semibold'>INFOS PRATIQUES</p>   
-              <p>A propos</p>   
-              <p>Livraisons & Reprises</p>   
-              <p>Mode d'emploi</p>   
-              <p>F.A.Q</p>   
-
-            </div>
-
-            <div className='text-sm text-[9px]'>
-              <p className=' text-[11px] font-semibold'>LEGAL</p>   
-              <p>Mentions Legales</p>   
-              <p>CGU</p>   
-              <p>CGV</p>   
-              <p>Politique de confidentialite</p>   
-
-            </div>
-            <div className='text-sm text-[9px]'>
-              <p className=' text-[11px] font-semibold'>MON COMPTE</p>   
-              <p>Acceder a mon compte</p>   
-              <p>Ma liste d'envie</p>   
-              <p>Creer un compte</p>   
-              <p>Mot de passe oublie</p>   
-
-            </div>
-          
-
+          <div className="flex mt-5 justify-around">
+            <input
+              type="text"
+              className="w-[60%] h-[50px] rounded-md placeholder:pl-4"
+              placeholder="john@doe.com"
+            />
+            <button className="bg-[#42ccd9] text-white font-semibold p-3 rounded-lg">
+              S&#39;INSCRIRE {`->`}
+            </button>
           </div>
-          </div> 
+        </div>
+      </div>
 
-          <div className='flex flex-col mb-10  items-center w-[40%] mr-10 gap-2'>
+      <div className="mt-10 border-t-2 pt-4 h-[400px] flex flex-col w-[100%] items-center gap-10 justify-between">
+        <div className="flex w-[100%] flex-col items-center gap-4">
+          <Image
+            width={300}
+            height={100}
+            alt="Website logo"
+            src="/logowebsie.png"
+            className="w-[100px] object-contain h-[100px]"
+          />
 
-            <p className='font-semibold'>NOUS SUIVRE</p>
-            <Image   width={100}    alt="image6"
-                    height={100} src='/footericons.png'      />
-              
+          <div className="flex w-[80%] justify-evenly gap-3 text-[10px]">
+            <div className="text-sm text-[9px]">
+              <p className="text-[11px] font-semibold">INFOS PRATIQUES</p>
+              <p>A propos</p>
+              <p>Livraisons & Reprises</p>
+              <p>Mode d&#39;emploi</p>
+              <p>F.A.Q</p>
+            </div>
+
+            <div className="text-sm text-[9px]">
+              <p className="text-[11px] font-semibold">LEGAL</p>
+              <p>Mentions Légales</p>
+              <p>CGU</p>
+              <p>CGV</p>
+              <p>Politique de confidentialité</p>
+            </div>
+            <div className="text-sm text-[9px]">
+              <p className="text-[11px] font-semibold">MON COMPTE</p>
+              <p>Accéder à mon compte</p>
+              <p>Ma liste d&#39;envie</p>
+              <p>Créer un compte</p>
+              <p>Mot de passe oublié</p>
+            </div>
           </div>
-          
-
-
         </div>
 
+        <div className="flex flex-col mb-10 items-center w-[40%] mr-10 gap-2">
+          <p className="font-semibold">NOUS SUIVRE</p>
+          <Image
+            width={100}
+            height={100}
+            alt="Footer icons for social media"
+            src="/footericons.png"
+          />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Page3
+export default Page3;
